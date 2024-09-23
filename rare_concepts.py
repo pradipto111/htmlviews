48029ff3e4d2f18407770be959e047b2a524f57c
pradipto111 <pravsman111@gmail.com>
2024-09-23 15:20:22 +0530

    added red cub classes

--- rare_concepts.py
+++ rare_concepts.py
@@ -10,17 +10,16 @@ src = './data/cub/'
 
 update = date.today()
 
-red_classes = ['Gymnastic_horizontal_bar',
-               'tarantula',
-               'madagaskar_cat',
-               'Rock_Beauty',
-               'grasshopper',
-               'mousetrap',
-               'mitten',
-               'Saharan_horned_viper',
-               'barracouta',
-               'Payphone',
-               'unicycle',]
+red_classes = ['Brandt_s_Cormorant',
+               'Spotted_Catbird',
+               'Red_faced_Cormorant',
+               'Chuck_will_s_widow',
+               'Parakeet_Auklet',
+               'Whip_poor_will',
+               'White_necked_Raven',
+               'Pomarine_Jaeger',
+               'Nelson_s_Sparrow',
+               'Least_Auklet']
 
 with open("renders/rare_concepts_cub.html", "w") as f:
     print(template.render(classes = class_names, red_classes = red_classes, update = str(update)), file = f)
