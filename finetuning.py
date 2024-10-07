0eb30dce216cae227ed4c45f451be2410b3bab36
pradipto111 <pravsman111@gmail.com>
2024-10-07 17:47:46 +0530

    added lora outputs

--- finetuning.py
+++ finetuning.py
@@ -2,7 +2,7 @@ from jinja2 import Environment, FileSystemLoader
 import os 
 from datetime import date
 env = Environment(loader = FileSystemLoader('templates'))
-template = env.get_template('custom_diffusion_outputs.jinja')
+template = env.get_template('finetuning_outputs.jinja')
 
 class_names = os.listdir('./data/sd2_ft')
 
@@ -10,5 +10,5 @@ update = date.today()
 
 red_classes = []
 
-with open("renders/custom_diffusion.html", "w") as f:
+with open("renders/finetuning_outputs.html", "w") as f:
     print(template.render(classes = class_names, red_classes = red_classes, update = str(update)), file = f)
